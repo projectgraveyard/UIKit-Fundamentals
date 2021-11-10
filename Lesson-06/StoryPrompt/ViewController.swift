9f93f7f85d767edfa42c3188ab2adf99c7c332ab
Dominic Swaine <dominic.swaine@transferwise.com>
2021-11-10 18:01:04 +0000

    Complete lesson 6 (Understand IBAction, IBOutlet)
    Co-Authored-By: Dominic Swaine <dom@dominicswaine.com>

--- Lesson-06/StoryPrompt/ViewController.swift
+++ Lesson-06/StoryPrompt/ViewController.swift
@@ -34,15 +34,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
+    @IBOutlet weak var nounTextField: UITextField!
+    @IBOutlet weak var adjectiveTextField: UITextField!
+    @IBOutlet weak var verbTextField: UITextField!
+    @IBOutlet weak var numberSlider: UISlider!
+    @IBOutlet weak var numberLabel: UILabel!
+    
     let storyPrompt = StoryPromptEntry()
-    storyPrompt.noun = "toaster"
-    storyPrompt.adjective = "smelly"
-    storyPrompt.verb = "burps"
-    storyPrompt.number = 10
-    print(storyPrompt)
-  }
+    
+    @IBAction func changeNumber(_ sender: UISlider) {
+        numberLabel.text = "Number \(Int(sender.value))"
+        storyPrompt.number = Int(sender.value)
+    }
+    
+    @IBAction func changeStoryType(_ sender: UISegmentedControl) {
+        if let genre = StoryPrompts.Genre(rawValue: sender.selectedSegmentIndex) {
+            storyPrompt.genre = genre
+        } else {
+            storyPrompt.genre = .scifi
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        numberSlider.value = 7.5
+        storyPrompt.noun = "toaster"
+        storyPrompt.adjective = "smelly"
+        storyPrompt.verb = "burps"
+        storyPrompt.number = Int(numberSlider.value)
+        print(storyPrompt)
+    }
 
 
 }
